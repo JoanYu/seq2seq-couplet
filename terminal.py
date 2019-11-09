4a76a3eee6065beef48a3635311b5ca3d2685d01
Zhengyang Yu <JoanYu@users.noreply.github.com>
2019-11-09 14:06:36 +0800

    修改一些低级的语法错误。

--- terminal.py
+++ terminal.py
@@ -8,12 +8,12 @@ m = Model(
         num_units=1024, layers=4, dropout=0.2,
         batch_size=32, learning_rate=0.0001,
         output_dir=model_dir,
-        restore_model=Ture, init_train=False, init_infer=True)
+        restore_model=True, init_train=False, init_infer=True)
 
-det chat_couplet(in_str):
+def chat_couplet(in_str):
     if len(in_str) == 0 or len(in_str) > 50:
         output = u'您的输入太长了'
-    else
+    else:
         output = m.infer(' '.join(in_str))
         output = ''.join(output.split(' '))
     print('上联：%s；下联：%s' % (in_str, output))
